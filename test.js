bece9ff632828af56585f01db76240ae04c59c3c
QueenE1337 <emmaradhall@wlanguest137173.eduroam.hj.se>
2018-05-22 14:19:00 +0200

    Saved fredriks tips

---
+++ test.js
@@ -0,0 +1,31 @@
+//When registrating: Loop through an array with users...
+
+//Global array to store the true/false - have we found a correct login?
+var found = false;
+
+//For-loop, going through the arrays users
+for (var i=0; < data.length; i++) {
+
+  if ( data[i].username == written_username && data[i].pass == written_password ) {
+    found = true;
+    break;
+  }
+}
+
+if (fround == true ) {
+  USER IS FOUND.
+}
+
+
+
+
+//Next example, using CHANGE:
+
+//First, make sure the HTML document is loaded:
+$(document).ready(function() {
+
+  $("#section").change(function() {
+    $("$form").toggleClass("moved");
+  });
+
+});
