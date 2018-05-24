c5522fde6d0efbf653927afa6d9713b2b72e0810
karinnlinnea <32543559+karinnlinnea@users.noreply.github.com>
2018-05-24 10:32:58 +0200

    Functioning hamburger menu

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ var transEnd = "transitionend webkitTransitionEnd oTransitionEnd otransitionend 
 
 $(document).ready(function() {
 
+	$("a#loginmenulink").on("click", function(e) {
+		e.preventDefault();
 
+		$("body").addClass("modal-showing");
+	});
 
 	// Open the login modal
 	$("a.open-modal").on("click", function(e) {
